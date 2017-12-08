535943fee8f21ec1c8f06846ad42d04d7142625b
Josh <gillingja@gmail.com>
2017-12-08 11:32:38 +1000

    Import Sparkline and implement charts in WeatherList

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import Chart from '../Components/chart';
+
 class WeatherList extends Component {
   renderWeather(cityData) {
-    const name= cityData.city.name;
+    const name = cityData.city.name;
+    const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp - 273.15);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
 
     return (
       <tr>
         <td>{name}</td>
+        <td>
+          <Chart data={temps} color="orange" units="C" />
+        </td>
+        <td>
+          <Chart data={pressures} color="purple" units="hPa" />
+        </td>
+        <td>
+          <Chart data={humidities} color="black" units="%" />
+        </td>
       </tr>
     )
   }
@@ -18,9 +32,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (C)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>
